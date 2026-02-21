6520f233de9b5d64c320bd91f82c232aa0d6addd
banskotanup <baskota.anup11@gmail.com>
2026-02-22 05:24:08 +0545

    2.11 exercise completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import Filter from './Filter';
-import PersonForm from './PersonForm';
-import Persons from './Persons';
+import { useEffect, useState } from "react";
+import Filter from "./Filter";
+import PersonForm from "./PersonForm";
+import Persons from "./Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '9876543210', id: 1 },
-    { name: 'Anup Banskota', number: '9876543210', id: 2 },
-    { name: 'Sanam Mishra', number: '9876543210', id: 3 },
-  ]) 
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [filterName, setFilterName] = useState('');
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterName, setFilterName] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   const personsToShow = persons.filter((p) => {
     return p.name.toLowerCase().includes(filterName.toLowerCase());
@@ -19,7 +25,7 @@ const App = () => {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    if (persons.some(p => p.name === newName)) {
+    if (persons.some((p) => p.name === newName)) {
       alert(`${newName} is already added to phonebook.`);
       return;
     }
@@ -27,34 +33,40 @@ const App = () => {
       name: newName,
       number: newNumber,
       id: persons.length + 1,
-    }
+    };
     setPersons(persons.concat(personObject));
     setNewName("");
     setNewNumber("");
-  }
+  };
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
-  }
+  };
 
   const handleNumberChange = (e) => {
     setNewNumber(e.target.value);
-  }
+  };
 
   const handleInputChange = (e) => {
     setFilterName(e.target.value);
-  }
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter value={filterName} onChange={ handleInputChange } />
+      <Filter value={filterName} onChange={handleInputChange} />
       <h2>add a new</h2>
-      <PersonForm handleFormSubmit={handleFormSubmit} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={ handleNumberChange} />
+      <PersonForm
+        handleFormSubmit={handleFormSubmit}
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      <Persons personsToShow={ personsToShow} />
+      <Persons personsToShow={personsToShow} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
